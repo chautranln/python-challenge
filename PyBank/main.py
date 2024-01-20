26421541c20b22ffed94880c6bfbc1a70553723d
chautranln <150863727+chautranln@users.noreply.github.com>
2024-01-20 10:58:48 -0500

    second iterative save

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,9 +10,11 @@ BudgetDataCSV = os.path.join("/Users/chaunguyen/Desktop/classwork/Homework/pytho
 
 #Set variables
 TotalMonths = 0
+PreviousProfit = 0
 TotalProfit = []
 Changes = []
-PreviousProfit = 0
+Date = []
+
 
 #Open csv file
 with open(BudgetDataCSV, encoding='UTF-8') as Budgetcsvfile:
@@ -26,28 +28,33 @@ with open(BudgetDataCSV, encoding='UTF-8') as Budgetcsvfile:
         TotalMonths += 1
 
 #Calculate net total of profit
+        date = row[0]
         profit = (int(row[1]))
         TotalProfit.append(profit)
+        Date.append(date)
 
 #Calculate changes in "Profit/Losses" over entire period, and then average of those changes
         change = (profit - PreviousProfit)
         Changes.append(change)
         PreviousProfit = profit
-    AverageChange = sum(Changes)/len(Changes)
+        AverageChange = sum(Changes)/(TotalMonths)
 
 #Greatest increase/decrease in profits (date and amount) 
-GreatestIncrease = max(TotalProfit)
-GreatestDecrease = min(TotalProfit)   
-
+GreatestIncrease = max(Changes)
+GreatestDecrease = min(Changes)
+GreatestIncreaseDate = Date[Changes.index(GreatestIncrease)]
+GreatestDecreaseDate = Date[Changes.index(GreatestDecrease)]
 
 #Output into text file
 #OutputPath = os.path.join("..", "analysis", "Bank_Data_Output.txt")
 
-with open(OutputPath, "w") as OutputFile:
+Output = os.path.join("Pybank", "analysis", "Budget_Output.txt")
+
+with open(Output, "w") as OutputFile:
     OutputFile.write("Financial Analysis\n")
     OutputFile.write("----------------------------\n")
-    OutputFile.write(f"Total Months: (TotalMonths)\n")
+    OutputFile.write(f"Total Months: {TotalMonths}\n")
     OutputFile.write(f"Total: ${sum(TotalProfit)}\n")
     OutputFile.write(f"Average Change: ${AverageChange}\n")
-    OutputFile.write(f"Greatest Increase in Profits: (${GreatestIncrease})\n")
-    OutputFile.write("Greatest Decrease in Profits: (${GreatestDecrease})\n")
+    OutputFile.write(f"Greatest Increase in Profits: {GreatestIncreaseDate} (${GreatestIncrease})\n")
+    OutputFile.write(f"Greatest Decrease in Profits: {GreatestDecreaseDate} (${GreatestDecrease})\n")
